395587b36a9a73a138c0c676447b7acdb0ee2587
Sinjinos <85186675+Sinjinos@users.noreply.github.com>
2024-04-15 00:39:56 +0200

    Update Message.jsx
    Commentaire

--- Front/src/Message.jsx
+++ Front/src/Message.jsx
@@ -1,18 +1,18 @@
 import styles from './Css/Message.module.css';
 import { Link } from 'react-router-dom';
 import TimeAgo from 'react-timeago';
-import { HiTrash } from "react-icons/hi";
+import { HiTrash } from "react-icons/hi"; //Importation de l'icône de poubelle pour le bouton de suppression
 import Api from './ApiCalls.js';
 
 
 
 function Message(props) {
 
-  const { message, date, author_name, _id, title} = props.Message;
-  const { nbMax } = props;
+  const { message, date, author_name, _id, title} = props.Message; //Destructuration pour obtenir les détails du message depuis les props
+  const { nbMax } = props; //Nombre maximal de caractères à afficher avant la troncature
 
   let msg = message;
-  if (nbMax > 0 && message.length > nbMax)
+  if (nbMax > 0 && message.length > nbMax) //Tronque le message si plus long que nbMax
     msg = message.slice(0, nbMax) + ' [...]';
 
   return (
