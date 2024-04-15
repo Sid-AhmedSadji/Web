004776b2a7bcb87ee8b91d8217f85ad59fa19b0d
Sinjinos <85186675+Sinjinos@users.noreply.github.com>
2024-04-15 02:24:05 +0200

    Update StylisedLabel.jsx
    Commentaire

--- Front/src/StylisedLabel.jsx
+++ Front/src/StylisedLabel.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import styles from './Css/StylisedLabel.module.css'
 
+//Définition du composant SearchInput qui prend 'value' et 'setValue' comme props
 const SearchInput = ({ value, setValue }) => (
-  <>
-    <input
-      type="text"
-      className={styles.inputSearch}
-      id="input-search"
-      value={value}
+  <>  {/* Fragment React pour regrouper plusieurs éléments sans ajouter de nœud supplémentaire au DOM */}
+    <input //Champ de saisie pour la recherche
+      type="text" //Définit le type de l'input comme texte
+      className={styles.inputSearch} //Application de styles CSS
+      id="input-search" //ID pour associer le label
+      value={value} //La valeur du champ est contrôlée par la prop 'value'
       onChange={(e) => setValue(e.target.value)} // Use an arrow function to update the value
     />
-    <label className={styles.search} htmlFor="input-search"></label>
+    <label className={styles.search} htmlFor="input-search"></label> {/* Label stylisé pour le champ de saisie */}
   </>
 );
 
