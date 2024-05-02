50b201bac66cb7f94e5bffdd63d6f0c94bfca4fe
Sinjinos <85186675+Sinjinos@users.noreply.github.com>
2024-05-02 14:28:49 +0200

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,29 @@
-const app = require("./app.js"); //Importe l'instance d'Express configurée depuis le fichier app.js
+const app = require("./app.js").default;; //Importe l'instance d'Express configurée depuis le fichier app.js
 const port = 4000; //Définit le port sur lequel le serveur doit écouter
+const WebSocket = require('ws'); //Importe la bibliothèque
 
 //Lance le serveur pour écouter sur le port spécifié
-app.default.listen(port, () => {
-  console.log(`Serveur actif sur le port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`Serveur démarré sur http://localhost:${port}`);
 });
 
-//Ce fichier est utilisé pour démarrer le serveur web, afin qu'il écoute sur le port 4000. Mais il ne faudait pas plutôt, le port 5173 pour cela ? 
+const wss = new WebSocket.Server({ server }); //Créer une nouvelle WebSocket
+
+wss.on('connection', function connection(ws) {
+  console.log('Un client WebSocket s\'est connecté.');
+
+  ws.on('message', function incoming(message) {
+    console.log('reçu: %s', message);
+
+    //Diffusion du message à tous les clients connectés
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => console.log('Un client WebSocket s\'est déconnecté.'));
+});
+
+//Ce fichier est utilisé pour démarrer le serveur web, afin qu'il écoute sur le port 4000.
